8f06146c372229c406886cd52586d165fd44a0a9
Daniil <sitnikovdan13@gmail.com>
2021-03-10 09:39:19 +0300

    feat: add search events

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,22 @@
-import React, {useState} from "react";
+import React, {MouseEventHandler, useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import style from './Header.module.scss';
 import SearchCountry from "./searchCountry/SearchCountry";
 import SelectLang from "./selectLang/SelectLang";
 
 const Header = () => {
-    const [inputStatus, setInputStatus] = useState(false);
+  const [closeStatus, setStatus] = useState(false);
+
+  const closeIt = () => {
+    setStatus(!closeStatus);
+  };
 
     return(
         <header className={style.header}>
             <Link to="/">Back to main page</Link>
-            <SearchCountry />
+            {!closeStatus && <SearchCountry />}
             <SelectLang />
+            <button onClick={closeIt}>cleaning</button>
         </header>
     );
 };
--- src/components/Header/searchCountry/SearchCountry.tsx
+++ src/components/Header/searchCountry/SearchCountry.tsx
@@ -1,8 +1,25 @@
-import React, {useRef} from "react";
+import React, {useEffect, useRef} from "react";
 import style from './searchCountry.module.scss';
 
 const SearchCountry = () => {
   const inputValue = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const keyboardHandler = (e: KeyboardEvent) => {
+      const { key } = e;
+      if (key === 'Enter') {
+        if (inputValue && inputValue.current) {
+          console.log(inputValue.current.value);
+        }
+      }
+    };
+
+    document.addEventListener('keydown', keyboardHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyboardHandler);
+    };
+  });
 
   const cleanContent = () => {
     if (inputValue && inputValue.current) {
@@ -10,13 +27,19 @@ const SearchCountry = () => {
     }
   };
 
+  const searchCountry = () => {
+    if (inputValue && inputValue.current) {
+      console.log(inputValue.current.value);
+    }
+  };
+
   return (
     <div className={style.searchCountry}>
       <div className={style.inputValue}>
-        <input ref={inputValue} type="text" autoFocus={true} placeholder="something here" />
+        <input autoComplete='off' ref={inputValue} type="text" autoFocus={true} placeholder="something here" />
         <span onClick={cleanContent} className={style.deleteValue}>X</span>
       </div>
-      <button className={style.acceptValue}>Search country</button>
+      <button onClick={searchCountry} className={style.acceptValue}>Search country</button>
     </div>
   );
 };
