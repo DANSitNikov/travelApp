06b733f2921693d542951e3215894bb7c724b539
Bogdan <big_bogdan@rambler.ru>
2021-03-11 12:09:21 +0300

    feat: added font, started setting up country page, installed package with youtube video into app

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { CountryTypes, RootState } from '../../types';
+import ReactPlayer from "react-player";
 
 function Country() {
   const { code } = useParams<{ code: string }>();
@@ -14,21 +15,39 @@ function Country() {
     return state.countries;
   });
 
+  const [content, setContent] = useState(1);
+
   useEffect(() => {
-    const currentCoutry:
+    const currentCountry:
       | CountryTypes
       | undefined = countriesArray.find((el: CountryTypes) => {
       return el.alpha3Code === code;
     });
-    if (currentCoutry) {
-      setCountryName(currentCoutry.name);
+    if (currentCountry) {
+      setCountryName(currentCountry.name);
     }
   }, [countriesArray, code]);
 
   return (
     <div className='countryPage'>
-      Test country with code: {code}
-      <p>{countryName}</p>
+      <div className='countryBody'>
+          <div className='contentSelector'>
+              <button onClick={() => {setContent(1)}}>1</button>
+              <button onClick={() => {setContent(2)}}>2</button>
+              <button onClick={() => {setContent(3)}}>3</button>
+              <button onClick={() => {setContent(4)}}>4</button>
+          </div>
+          <div className='countryContent'>
+              <div className='countryName'>
+                  This is {countryName}.
+              </div>
+              <ReactPlayer
+                  url="https://www.youtube.com/watch?v=05TfxysYZOo"
+                  controls={true}
+                  volume={0.5}
+              />
+          </div>
+      </div>
     </div>
   );
 }
