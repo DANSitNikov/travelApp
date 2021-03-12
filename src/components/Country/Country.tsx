b0d2ac6cbab102c906658d7691e163c800522868
Snopok Stanislav <snopok.stanislav@mail.ru>
2021-03-13 00:18:48 +0300

    feat: add weather widget without styles

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,5 +1,6 @@
 import './Country.scss';
 import { useParams } from 'react-router-dom';
+import WeatherWidget from '../WeatherWidget'
 
 interface Props {
   code: string;
@@ -9,7 +10,12 @@ function Country() {
   const { code }: Props = useParams();
 
   return (
-    <div className='countryPage'>Test country with code: {code}</div>
+    <>
+      <div className='countryPage'>Test country with code: {code}
+        <WeatherWidget city={'Washington'} />
+      </div>
+    </>
+
     // в этом компоненте нужно делать новый асихронный запрос через редакс по коду
   );
 }
---
+++ src/components/WeatherWidget/WeatherWidget.tsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import './WeatherWidget.scss'
+
+
+interface weatherProps {
+    city: string
+}
+
+const WeatherWidget: React.FC<weatherProps> = (props) => {
+    const [weatherIcon, setWeatherIcon] = useState('')
+    const [temperature, setTemperature] = useState('')
+    const [weatherDescription, setWeatherDescription] = useState('')
+
+    useEffect(() => {
+        async function getWeather() {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&lang=en&appid=55aa85f147056ca0778f19550850090c&units=metric`;
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data.weather);
+            setWeatherIcon(`${data.weather[0].icon}`)
+            setTemperature(`${data.main.temp}`)
+            setWeatherDescription(`${data.weather[0].description}`)
+        }
+        getWeather()
+    }, [props.city])
+
+    return (
+        <div className='weather_widget'>
+            <div className='weather_icon'><img src={`http://openweathermap.org/img/w/${weatherIcon}.png`} alt={weatherDescription} /></div>
+            <div className='weather_temperature'>{temperature}°С</div>
+            <div className='weather_description'>{weatherDescription}</div>
+        </div>
+    )
+}
+
+export default WeatherWidget
