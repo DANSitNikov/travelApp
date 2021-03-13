8fb34196e958c6bc78a27762550232181dfb3d5d
Snopok Stanislav <snopok.stanislav@mail.ru>
2021-03-14 00:02:01 +0300

    feat: add map with country and capital mark

--- src/components/Country/Country.tsx
+++ src/components/Country/Country.tsx
@@ -1,5 +1,7 @@
 import './Country.scss';
 import { useParams } from 'react-router-dom';
+import CountryMap from '../CountryMap'
+import countryGeo from '../CountryMap/DEU.geo.json'
 
 interface Props {
   code: string;
@@ -9,7 +11,10 @@ function Country() {
   const { code }: Props = useParams();
 
   return (
-    <div className='countryPage'>Test country with code: {code}</div>
+    <div className='countryPage'>Test country with code: {code}
+      <CountryMap capitalGeo={[52.50, 13.38]} capitalName='Berlin' countryGeo={countryGeo} />
+    </div>
+
     // в этом компоненте нужно делать новый асихронный запрос через редакс по коду
   );
 }
