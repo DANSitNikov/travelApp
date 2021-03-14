51d26389c7646787dc41e1f910c881b02ee6afd1
Bogdan <big_bogdan@rambler.ru>
2021-03-14 14:57:36 +0300

    feat: made 2nd version of loader

--- src/components/Country/Loader/Loader.tsx
+++ src/components/Country/Loader/Loader.tsx
@@ -1,23 +1,17 @@
 import React from "react"
-import ContentLoader from "react-content-loader"
+import './Loader.scss'
 
 function Loader() {
     return (
-        <div>
-            <ContentLoader
-                speed={2}
-                width={'81vw'}
-                height={'47vw'}
-                viewBox="0 0 500 364"
-                backgroundColor="#f3f3f3"
-                foregroundColor="#ecebeb"
-            >
-                <rect x="15" y="4" rx="0" ry="0" width="519" height="37" />
-                <rect x="132" y="56" rx="0" ry="0" width="260" height="133" />
-                <rect x="10" y="231" rx="0" ry="0" width="87" height="57" />
-                <rect x="117" y="216" rx="0" ry="0" width="324" height="40" />
-                <rect x="50" y="276" rx="0" ry="0" width="0" height="13" />
-            </ContentLoader>
+        <div className='loadingBlock'>
+            <h2 className='loadingText'>
+                Loading
+            </h2>
+            <div className="spinner">
+                <div className="spinner-item"></div>
+                <div className="spinner-item"></div>
+                <div className="spinner-item"></div>
+            </div>
         </div>
     );
 }
